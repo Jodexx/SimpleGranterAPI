42da313984b1bff5b8c4ce6a24088b2c7488bf20
JodexIndustries <0984134q@gmail.com>
2023-06-25 16:01:37 +0300

    1.0.0

---
+++ src/main/java/com/jodexindustries/simplegranter/api/GranterAPI.java
@@ -0,0 +1,52 @@
+package com.jodexindustries.simplegranter.api;
+
+import org.bukkit.entity.Player;
+
+public class GranterAPI {
+    /**
+     * Check if the player can give groups.
+     * @param player A player who gives
+     * @return Boolean
+     */
+    public boolean isPlayerCanGrantGroups(Player player) {
+        return isPlayerCanGrantGroups(player);
+    }
+
+    /**
+     * Check if the group exists
+     * @param group Group name
+     * @return Boolean
+     */
+    public boolean isGroupExist(String group) {
+        return isGroupExist(group);
+    }
+
+    /**
+     * Get group level
+     * @param group Group name
+     * @return Group level
+     */
+    public int getGroupLevel(String group) {
+        return getGroupLevel(group);
+    }
+
+    /**
+     * Get the number of groups a group can give.
+     * @param senderGroup The group of the giving player
+     * @param group The group the player wants to give
+     * @return Number of groups
+     */
+    public int getGroupInConfig(String senderGroup, String group) {
+        return getGroupInConfig(senderGroup, group);
+    }
+
+    /**
+     * Get the number of groups that the player can still give.
+     * @param player A player who gives
+     * @param group The group the player wants to give
+     * @return Number of groups
+     */
+    public int getGroupInData(Player player, String group) {
+        return getGroupInData(player, group);
+    }
+}
